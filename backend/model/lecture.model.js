fc46d1ec4958c736f837d508acc70090fe8a86ad
aamirkhan9420 <aamirkhan.z9373@gmail.com>
2023-02-06 11:58:22 +0530

    lecture  model added

---
+++ backend/model/lecture.model.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose')
+const lectureschema=mongoose.Schema({
+
+    topic_name:String,
+    lecture_date:String,
+    lecture_time:String,
+    teacher_name:String,
+    lecture_id:String,
+    lecture_type:String,
+    userId:String
+
+})
+const LectureModel=mongoose.model("lecture",lectureschema)
+module.exports={LectureModel}
